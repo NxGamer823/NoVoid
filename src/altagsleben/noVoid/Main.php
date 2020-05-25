88d2b8054eebf9b7d3675d83f5608f17fc267409
SirvGamer <55999189+altagsleben@users.noreply.github.com>
2020-05-25 18:50:19 +0200

    Add files via upload

---
+++ src/altagsleben/noVoid/Main.php
@@ -0,0 +1,36 @@
+<?php
+namespace altagsleben\NoVoid;
+    use pocketmine\plugin\PluginBase;
+    use pocketmine\event\Listener;
+    use pocketmine\level\Position;
+    use pocketmine\level\Level;
+    use pocketmine\Player;
+    use pocketmine\entity\Entity;
+    use pocketmine\math\Vector3;
+    use pocketmine\event\player\PlayerMoveEvent;
+    
+class Main extends PluginBase implements Listener{
+    
+public function onEnable(){
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+}
+
+public function checkVoid(PlayerMoveEvent $event){
+    $player = $event->getPlayer();
+    $x = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getFloorX();
+    $y = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getFloorY();
+    $z = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getFloorZ();
+    $level = $this->getServer()->getDefaultLevel();
+        if($event->getTo()->getFloorY() < 0){
+            switch(mt_rand(1, 2) == 1){
+              case 1:
+              $player->teleport(new Position($x, $y, $z, $level));
+              $player->setHealth($player->getHealth(20));
+              $player->sendMessage("§c§lSystem §r§7» §r§aYou were saved!");
+              break;
+              case 2:
+              break;
+             }
+         }
+      }
+}
